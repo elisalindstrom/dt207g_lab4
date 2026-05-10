e3c6b416c289cb2c603709fa7192c8f11bd1aa63
elisalindstrom <elisalindstroem@gmail.com>
2026-05-10 16:19:04 +0200

    skapat schema och model + lagt till routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,4 +13,40 @@ router.get("/", async (req, res) => {
     }
 })
 
+// Registrering
+router.post("/register", async (req, res) => {
+    try {
+        const { username, password } = req.body; // Data från request body
+
+        // Validering
+        if (!username || !password) return res.status(400).json({ message: "Username and password required" });
+
+        const user = new User({ username, password });
+        await user.save();
+
+        return res.status(201).json({ message: "User created" });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+})
+
+router.post("/login", async (req, res) => {
+    try {
+        const { username, password } = req.body; // Data från request body
+
+        // Validering
+        if (!username || !password) return res.status(400).json({ message: "Username and password required" });
+
+        const user = await User.findOne({ username });
+        if (!user) return res.status(401).json({ error: "Invalid username or password" });
+
+        const isPasswordMatch = await user.comparePassword(password);
+        if (!isPasswordMatch) return res.status(401).json({ error: "Invalid username or password" });
+
+        return res.status(200).json({ message: "User logged in" })
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+})
+
 module.exports = router;
